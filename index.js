71670c87b83ef1aa6bae14f866e949ab03b32360
Tejasreeobili <161826124+Tejasreeobili@users.noreply.github.com>
2024-05-04 12:20:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var randomNumber1=Math.random();
 randomNumber1=Math.floor(randomNumber1*6)+1;
 var randomDice="dice"+randomNumber1+".png";
-var randomImageSource="/images/"+randomDice;
+var randomImageSource="./images/"+randomDice;
 
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImageSource);
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomImageSource2="/images/dice"+randomNumber2+".png";
+var randomImageSource2="./images/dice"+randomNumber2+".png";
 var image2=document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomImageSource2);
 if(randomNumber1>randomNumber2){
