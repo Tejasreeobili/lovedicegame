78bc394d2744075720a696b4d07c3aa64d231cfb
Tejasreeobili <161826124+Tejasreeobili@users.noreply.github.com>
2024-05-04 12:14:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1=Math.random();
+randomNumber1=Math.floor(randomNumber1*6)+1;
+var randomDice="dice"+randomNumber1+".png";
+var randomImageSource="/images/"+randomDice;
+
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImageSource2="/images/dice"+randomNumber2+".png";
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩player1 won";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="player2🚩 won";
+}else{
+    document.querySelector("h1").innerHTML="draw";
+}
+
+
+
+
